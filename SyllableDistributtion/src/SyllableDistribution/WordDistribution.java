2977a471a56c11b403442b561dc3eff2e4dff7f4
ducdan <ducdan1702@gmail.com>
2016-08-25 10:05:06 +0700

    [DanHD] change to completely parallel

--- SyllableDistributtion/src/SyllableDistribution/WordDistribution.java
+++ SyllableDistributtion/src/SyllableDistribution/WordDistribution.java
@@ -1,9 +1,10 @@
 package SyllableDistribution;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -12,23 +13,10 @@ import java.util.stream.Stream;
  */
 public class WordDistribution {
 
-    private void putMapUsingDefault(Object key, Map map) {
-        if (map.containsKey(key)) {
-            int value = (int) map.get(key) + 1;
-            map.put(key, value);
-        } else {
-            map.put(key, 1);
-        }
-    }
-
-    public Map<Diacritic, Integer> countTones(String s) {
-        Map<Diacritic, Integer> map = new HashMap<>();
-        String[] words = s.replaceAll("_", " ").split(" ");
-        Stream.of(words)
-                .parallel()
-                .map(w -> Diacritic.getTone(w))
-                .forEach(t -> putMapUsingDefault(t, map));
-        return map;
+    public Map<Diacritic, Long> countTones(Stream<String> words) {
+        return words.parallel()
+                .flatMap(w -> Arrays.stream(w.replaceAll("_", " ").split(" ")))
+                .collect(Collectors.groupingBy(w -> Diacritic.getTone(w), Collectors.counting()));
     }
 
     private int countMatch(String s, String regex) {
@@ -40,14 +28,9 @@ public class WordDistribution {
         return matches;
     }
 
-    public Map<Integer, Integer> countSyllables(String s, String regex) {
-        Map<Integer, Integer> result = new HashMap();
-        String[] words = s.split(" ");
-        Stream.of(words)
-                .parallel()
-                .map(w -> countMatch(w, regex))
-                .forEach(t -> putMapUsingDefault(t, result));
-        return result;
+    public Map<Integer, Long> countSyllables(Stream<String> words) {
+        return words.parallel()
+                .collect(Collectors.groupingBy(w -> countMatch(w, "_"), Collectors.counting()));
     }
 
 }
